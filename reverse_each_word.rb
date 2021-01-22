bbe8bd4bf4dfcc3a77dbb3dfb003b6b0f66d1c2b
Kyle Davis <davis.h.kyle@gmail.com>
2021-01-22 23:53:04 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,11 @@
-def reverse_each_word(string)
-  string_to_array = string.split(" ")
-  string_to_array.collect do |element|
-    element.reverse
-    end
-end
 
-string.split(" ").collect {|word| word.reverse!}.join(" ")
+
+def reverse_each_word (string)
+   string_to_array = string.split (" ")
+   sentence_array = string_to_array.collect do |element|
+     element.reverse
+     sentence_array.join( " " )
+     end
+   end 
+ end 
+
